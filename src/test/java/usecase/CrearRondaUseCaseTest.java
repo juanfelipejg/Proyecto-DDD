95352ea532b2b23f89c102cc808c32a7c216770b
jjaramga <juanfelipejg@gmail.com>
2021-03-14 16:55:53 -0500

    Create Test to use case Crear Ronda

---
+++ src/test/java/usecase/CrearRondaUseCaseTest.java
@@ -0,0 +1,34 @@
+package usecase;
+
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.support.TriggeredEvent;
+import domain.juego.events.JuegoIniciado;
+import domain.juego.values.JugadorId;
+import domain.ronda.events.RondaCreada;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CrearRondaUseCaseTest {
+
+    @Test
+    void crearRonda(){
+        var event = new JuegoIniciado(Set.of(JugadorId.of("xxxx"), JugadorId.of("ffff")));
+        event.setAggregateRootId("hhhh");
+        var useCase = new CrearRondaUseCase();
+
+        var events = UseCaseHandler
+                .getInstance()
+                .syncExecutor(useCase, new TriggeredEvent<>(event))
+                .orElseThrow()
+                .getDomainEvents();
+
+        var rondaCreada = (RondaCreada) events.get(0);
+        Assertions.assertEquals("hhhh", rondaCreada.getJuegoId().value());
+        Assertions.assertEquals(2, rondaCreada.getJugadorIds().size());
+    }
+
+}

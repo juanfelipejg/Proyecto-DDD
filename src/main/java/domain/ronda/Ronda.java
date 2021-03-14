a1a4d5128d156a0725c0de2dd976969c3dc81ca4
jjaramga <juanfelipejg@gmail.com>
2021-03-13 23:55:29 -0500

    Create value objects Cara and DadoId

---
+++ src/main/java/domain/ronda/Ronda.java
@@ -0,0 +1,16 @@
+package domain.ronda;
+
+import co.com.sofka.domain.generic.AggregateEvent;
+import domain.juego.values.JuegoId;
+import domain.juego.values.RondaId;
+import domain.ronda.values.DadoId;
+
+import java.util.Map;
+
+public class Ronda extends AggregateEvent<RondaId> {
+
+    protected JuegoId juegoId;
+    protected Map<DadoId, Dado> dados;
+    protected Map<EtapaId, Etapa> etapas;
+    protected Map<JuegoId, Punto> puntaje;
+}
--- src/main/java/usecase/CrearRondaUseCase.java
+++ src/main/java/usecase/CrearRondaUseCase.java
@@ -4,13 +4,18 @@ import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.ResponseEvents;
 import co.com.sofka.business.support.TriggeredEvent;
 import domain.juego.events.JuegoIniciado;
+import domain.juego.values.JuegoId;
+import domain.juego.values.RondaId;
+import domain.ronda.Ronda;
 
 public class CrearRondaUseCase extends UseCase<TriggeredEvent<JuegoIniciado>, ResponseEvents> {
     @Override
     public void executeUseCase(TriggeredEvent<JuegoIniciado> juegoIniciadoTriggeredEvent) {
 
         var event = juegoIniciadoTriggeredEvent.getDomainEvent();
-
+        var rondaId = new RondaId();
+        var juegoId = JuegoId.of(event.aggregateRootId());
+        var ronda = new Ronda
 
     }
 }

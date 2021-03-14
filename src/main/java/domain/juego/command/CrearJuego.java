9df19bb9893da3898043b31c935026bb19cf4ee0
jjaramga <juanfelipejg@gmail.com>
2021-03-14 13:18:55 -0500

    Modified command Crear Juego

--- src/main/java/domain/juego/command/CrearJuego.java
+++ src/main/java/domain/juego/command/CrearJuego.java
@@ -2,18 +2,28 @@ package domain.juego.command;
 
 import co.com.sofka.domain.generic.Command;
 import domain.juego.Jugador;
+import domain.juego.values.Capital;
+import domain.juego.values.JugadorId;
+import domain.juego.values.Nombre;
 
+import java.util.Map;
 import java.util.Set;
 
 public class CrearJuego implements Command {
 
-    private final Set<Jugador> jugadores;
+    private final Map<JugadorId, Capital> capitales;
+    private final Map<JugadorId, Nombre> nombres;
 
-    public CrearJuego(Set<Jugador> jugadores) {
-        this.jugadores = jugadores;
+    public CrearJuego(Map<JugadorId, Capital> capitales, Map<JugadorId, Nombre> nombres) {
+        this.capitales = capitales;
+        this.nombres = nombres;
     }
 
-    public Set<Jugador> getJugadores() {
-        return jugadores;
+    public Map<JugadorId, Capital> getCapitales() {
+        return capitales;
+    }
+
+    public Map<JugadorId, Nombre> getNombres() {
+        return nombres;
     }
 }

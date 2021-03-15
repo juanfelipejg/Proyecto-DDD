72446d095405b12f5607dbb1d7c73d4111a27b84
jjaramga <juanfelipejg@gmail.com>
2021-03-15 00:23:38 -0500

    Improve Entity Etapa

--- src/main/java/domain/ronda/Etapa.java
+++ src/main/java/domain/ronda/Etapa.java
@@ -6,19 +6,16 @@ import domain.ronda.values.Cara;
 import domain.ronda.values.Case;
 import domain.ronda.values.EtapaId;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Etapa extends Entity<EtapaId> {
 
-    private final Set<Cara> carasVisibles;
+    private final List carasVisibles;
     private final Map<JugadorId, Case> cases;
 
-    public Etapa(EtapaId entityId) {
+    public Etapa(EtapaId entityId, List<Cara> carasVisibles) {
         super(entityId);
-        this.carasVisibles = new HashSet<>();
+        this.carasVisibles = carasVisibles;
         this.cases = new HashMap<>();
     }
 
@@ -34,7 +31,7 @@ public class Etapa extends Entity<EtapaId> {
         return cases;
     }
 
-    public Set<Cara> carasVisibles() {
+    public List carasVisibles() {
         return carasVisibles;
     }
 }

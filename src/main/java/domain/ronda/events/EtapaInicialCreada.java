3e97db4b3af7b2a0dea6d89f96b8641482acadd6
jjaramga <juanfelipejg@gmail.com>
2021-03-15 00:46:13 -0500

    Modify event Etapa Inicial Creada

--- src/main/java/domain/ronda/events/EtapaInicialCreada.java
+++ src/main/java/domain/ronda/events/EtapaInicialCreada.java
@@ -1,20 +1,27 @@
 package domain.ronda.events;
 
 import co.com.sofka.domain.generic.DomainEvent;
+import domain.ronda.Etapa;
 import domain.ronda.values.Cara;
+import domain.ronda.values.EtapaId;
 
 import java.util.List;
 
-public class EtapaInicialIniciada extends DomainEvent {
+public class EtapaInicialCreada extends DomainEvent {
 
+    private final EtapaId etapaId;
     private final List<Cara> carasVisibles;
 
-    public EtapaInicialIniciada(List<Cara> carasVisibles) {
+    public EtapaInicialCreada(EtapaId etapaId, List<Cara> carasVisibles) {
         super("nomemientan.ronda.etapacreada");
+        this.etapaId = etapaId;
         this.carasVisibles = carasVisibles;
     }
-
     public List<Cara> getCarasVisibles() {
         return carasVisibles;
+    }
+
+    public EtapaId getEtapaId() {
+        return etapaId;
     }
 }

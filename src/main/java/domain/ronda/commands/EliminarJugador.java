6f04387b080ed672ccc3e58b39b5aa15ad585e5c
jjaramga <juanfelipejg@gmail.com>
2021-03-17 10:03:33 -0500

    Create command Eliminar jugador

---
+++ src/main/java/domain/ronda/commands/EliminarJugador.java
@@ -0,0 +1,39 @@
+package domain.ronda.commands;
+
+import co.com.sofka.domain.generic.Command;
+import domain.juego.values.Capital;
+import domain.juego.values.JuegoId;
+import domain.juego.values.JugadorId;
+import domain.juego.values.Nombre;
+
+public class EliminarJugador implements Command {
+
+    private final JuegoId juegoId;
+    private final JugadorId jugadorId;
+    private final Nombre nombre;
+    private final Capital capital;
+
+    public EliminarJugador(JuegoId juegoId, JugadorId jugadorId, Nombre nombre, Capital capital) {
+        this.juegoId = juegoId;
+        this.jugadorId = jugadorId;
+        this.nombre = nombre;
+        this.capital = capital;
+    }
+
+    public JuegoId getJuegoId() {
+        return juegoId;
+    }
+
+    public JugadorId getJugadorId() {
+        return jugadorId;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public Capital getCapital() {
+        return capital;
+    }
+
+}

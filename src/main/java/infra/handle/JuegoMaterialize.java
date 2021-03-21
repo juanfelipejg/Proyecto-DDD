5e56d6791ad5c20e171fc8d98f6a9beff4d4441c
jjaramga <juanfelipejg@gmail.com>
2021-03-20 21:25:03 -0500

    Create command Controller

--- src/main/java/infra/handle/JuegoMaterialize.java
+++ src/main/java/infra/handle/JuegoMaterialize.java
@@ -1,10 +1,20 @@
 package infra.handle;
 
+import co.com.sofka.domain.generic.DomainEvent;
+import domain.juego.events.JuegoCreado;
+import domain.juego.events.JugadorAdicionado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Component
@@ -16,4 +26,32 @@ public class JuegoMaterialize {
     @Autowired
     @Qualifier("mongoTemplateQueries")
     private MongoTemplate mongoTemplate;
+
+    @Async
+    @EventListener
+    public void handleEventJuegoCreado(JuegoCreado juegoCreado) {
+        logger.info("****** Handle event juegoCreado");
+        Map<String, Object> data = new HashMap<>();
+        data.put("_id", juegoCreado.getJuegoId().value());
+        data.put("isJuegoInicializado", false);
+        mongoTemplate.save(data, COLLECTION_NAME);
+    }
+
+    @Async
+    @EventListener
+    public void handleEventJugadorAdicionado(JugadorAdicionado jugadorAdicionado) {
+        logger.info("****** Handle event jugadorAdicionado");
+        Update update = new Update();
+        var id = jugadorAdicionado.getJugadorId().value();
+        update.set("jugadores."+id+".capital", jugadorAdicionado.getCapital().value());
+        update.set("jugadores."+id+".nombre", jugadorAdicionado.getNombre().value());
+
+        mongoTemplate.updateFirst(getFilterByAggregateId(jugadorAdicionado), update, COLLECTION_NAME);
+    }
+
+    private Query getFilterByAggregateId(DomainEvent event) {
+        return new Query(
+                Criteria.where("_id").is(event.aggregateRootId())
+        );
+    }
 }
